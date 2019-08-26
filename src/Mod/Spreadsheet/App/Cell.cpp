d1713b9c84bed852289f34d8130c0c1cf8060cdb
Zheng, Lei <realthunder.dev@gmail.com>
2019-08-26 12:09:09 +0800

    Expression: split Expression.h to ExpressionParser.h
    Split Expression details into a separate header to reduce recompilation
    time on changes.

--- src/Mod/Spreadsheet/App/Cell.cpp
+++ src/Mod/Spreadsheet/App/Cell.cpp
@@ -34,7 +34,7 @@
 #include <Base/Quantity.h>
 #include <Base/Writer.h>
 #include <Base/Console.h>
-#include <App/Expression.h>
+#include <App/ExpressionParser.h>
 #include "Sheet.h"
 #include <iomanip>
 
